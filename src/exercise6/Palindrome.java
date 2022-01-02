8f12839af556f54427da8f6d0b73b80718f37cb6
Boris Lilovski <92319563+Bobadon@users.noreply.github.com>
2022-01-02 12:17:24 +0300

    Update Palindrome.java

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -4,6 +4,26 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        int nextDig;
+        int sum = 0;
+        boolean pal = false;
+        int temp = num;
+
+        while(num > 0) {
+            nextDig = num % 10;
+            sum = (sum*10) + nextDig;
+            number /= 10;
+        }
+
+        if(temp == sum) {
+            pal = true;
+        }
+
+        System.out.println("Palindrome: " + pal);
     }
 }
